6b7929d40c953c34c9dab9960351210a2054f444
vmakarov <vmakarov@nomia.net>
2024-10-20 21:47:17 +0200

    fix nexus url

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,7 @@ publishing {
 
     repositories {
         maven {
-            url = uri(System.getenv("NEXUS_URL")) ?: throw IllegalArgumentException("URL mustn't be null")
+            url = uri(System.getenv("NEXUS_URL"))
             credentials {
                 username = System.getenv("NEXUS_USERNAME")
                 password = System.getenv("NEXUS_PASSWORD")
